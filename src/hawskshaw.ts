ce6c3cfdcdd80162c063d3722e46e1963a3f43cb
Tom Wilhelm <hex_machina@live.com>
2026-01-13 17:20:46 -0800

    package update

---
+++ src/hawskshaw.ts
@@ -0,0 +1,11 @@
+export class Hawkshaw {}
+
+module.exports = exports = Hawkshaw;
+module.exports.Ajv = Hawkshaw;
+Object.defineProperty(exports, "__esModule", { value: true });
+
+export default Hawkshaw;
+
+export { FileUtil } from "./utils/fileUtil";
+export { SchemaUtil } from "./utils/schemaUtil";
+export { ExportUtil } from "./utils/exportUtil";
