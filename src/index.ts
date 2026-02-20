dcca925ab57924a1d9004a053bc7d5e40033ab73
Tom Wilhelm <hex_machina@live.com>
2026-02-19 16:16:47 -0800

    use index

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,18 @@ import { ValidateCommand } from "./commands/validate";
 import { ImportSheetCommand } from "./commands/importSheet";
 import { CleanSheetCommand } from "./commands/cleanSheet";
 
+export class Hawkshaw {}
+
+module.exports = exports = Hawkshaw;
+module.exports.Hawkshaw = Hawkshaw;
+Object.defineProperty(exports, "__esModule", { value: true });
+
+export default Hawkshaw;
+
+export { FileUtil } from "./utils/fileUtil";
+export { SchemaUtil } from "./utils/schemaUtil";
+export { ExportUtil } from "./utils/exportUtil";
+
 const program = new Command();
 
 program
